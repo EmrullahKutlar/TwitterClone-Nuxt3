e754c5b88fd81636a582f132bba35eec089f35b5
EmrullahKutlar <emrullah.ktlr@gmail.com>
2022-10-04 17:11:50 +0300

    tweet ve mediafile db collectionları oluşturuldu

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,7 +4,8 @@ import { sendError } from "h3";
 import { getUserById } from "../db/users";
 
 export default defineEventHandler(async (event) => {
-  const endpoints = ["/api/auth/user"];
+  const endpoints = ["/api/auth/user",
+  "/api/user/tweets"];
   const isHandledByThisMiddleware = endpoints.some((endpoint) => {
     const pattern = new UrlPAttern(endpoint);
 
