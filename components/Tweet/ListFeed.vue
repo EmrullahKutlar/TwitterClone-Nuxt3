aa318cb911ba112dc7db74a5a673983cd042184c
EmrullahKutlar <emrullah.ktlr@gmail.com>
2022-10-10 22:13:27 +0300

    rply sonrası redirect eklendi

--- components/Tweet/ListFeed.vue
+++ components/Tweet/ListFeed.vue
@@ -6,7 +6,7 @@
     </div>
 
     <div v-else class=" pb-4 border-b hover:bg-gray-100 cursor-pointer dark:hover:bg-dim-300"
-      :class="[twitterBorderColor, defaultTransition]" v-for=" tweet in props.tweets" :key="tweet.id">
+      :class="[twitterBorderColor, defaultTransition]" v-for=" tweet in props.tweets" :key="tweet.id" @click.prevent="redirect(tweet)">
       <TweetItem :tweet="tweet" compact="true">
       </TweetItem>
     </div>
@@ -22,6 +22,9 @@ const props = defineProps({
 })
 const { twitterBorderColor, defaultTransition } = useTailwindConfig()
 const isEmptyArray = computed(() => props.tweets.length == 0)
+const redirect=(tweet)=>{
+navigateTo(`/status/${tweet.id}`)
+}
 </script>
 
 <style>
